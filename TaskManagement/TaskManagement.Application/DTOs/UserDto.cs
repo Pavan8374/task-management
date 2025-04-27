e319f79ad92a699ddae194f2724e4bf03d1ec3d4
Pavan8374 <rpavank2002@gmail.com>
2025-04-27 23:06:48 +0530

    added summary

--- TaskManagement/TaskManagement.Application/DTOs/UserDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TaskManagement.Application.DTOs
-{
-    public class UserDto
-    {
-    }
-}
--- TaskManagement/TaskManagement.Infrastructure/Persistence/AppDbContext.cs
+++ TaskManagement/TaskManagement.Infrastructure/Persistence/AppDbContext.cs
@@ -4,13 +4,35 @@ using TaskManagement.Domain.Entities;
 
 namespace TaskManagement.Infrastructure.Persistence
 {
+    /// <summary>
+    /// Application db context
+    /// </summary>
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// Application db context constructor
+        /// </summary>
+        /// <param name="options">options</param>
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        /// <summary>
+        /// Users
+        /// </summary>
         public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Roles
+        /// </summary>
         public DbSet<Role> Roles { get; set; }
+
+        /// <summary>
+        /// Tasks
+        /// </summary>
         public DbSet<Domain.Entities.Task> Tasks { get; set; }
+
+        /// <summary>
+        /// Task comments
+        /// </summary>
         public DbSet<TaskComment> TaskComments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
